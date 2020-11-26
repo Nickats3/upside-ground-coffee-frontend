12628f2baecde08707c730e19cbbce263da829fe
Kevin Steele <kevins.steele@gmail.com>
2020-11-25 17:49:22 -0800

    user address input working

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   var [orders, setOrders] = useState([]);
   var [order, setOrder] = useState({});
 
-
+//EXAMPLE SCHEMA FROM DATABASE
 //   {"_id":{"$oid":"5fa7574ead5ecf319043b04b"},
 //   "customer":{
 //     "firstname":"Kevin",
@@ -28,6 +28,12 @@ function App() {
 
   var [customer, setCustomer] = useState({});
   var [address, setAddress] = useState({});
+
+  var [street, setStreet] = useState("");
+  var [city, setCity] = useState("");
+  var [state, setState] = useState("");
+  var [zip, setZip] = useState("");
+
   var [product, setProduct] = useState("");
   var [price, setPrice] = useState(-1);
 
@@ -38,7 +44,7 @@ function App() {
 
   const getAllOrders = async () => {
     let ordersData = await fetch("http://localhost:3000/orders/");
-    let ord = await ordersData.json();
+    let ord = await ordersData.json(); //ord BECAUSE order AND orders ARE USED ABOVE ON LINES 8 AND 9
     setOrders(ord.data.orders)
   } 
 
@@ -56,34 +62,56 @@ const handleSubmit = async (e) => {
         email: "kevins.steele@gmail.com"
       },
       address:{
-        street: "1700 Pennsylvania Ave",
-        city: "Washington",
-        state: "DC",
-        zip: 17760
+        street: street,
+        city: city,
+        state: state,
+        zip: zip
       },
       product,
       price,
     })
   })
 
+  //why is this undefined if using address? Lines 76-80 are in place for testing the user inputs
+  console.log(address);
+  console.log(address.street);
+  console.log(address.city);
+  console.log(state);
+  console.log(zip);
+
   let newOrder = await newOrderData.json();
 
   getAllOrders();
 }
 
+// const newAddress = Object.assign({}, address);
+// const newCity = Object.assign({}, address.city)
+
   return (
     <div className="App">
         <form onSubmit={handleSubmit}>
           <label>Product</label>
-          <input type="text" onChange = { e => setProduct(e.target.value)} />
+          <input type="text" onChange = { e => setProduct(e.target.value)} /> 
           <label>Price</label>
           <input type="text" onChange = { e => setPrice(e.target.value)} />
+
+          <br></br>
+          <label>Street</label>
+          <input type="text" value = {street} onChange = { e => setStreet(e.target.value)} />
+          <label>City</label>
+          <input type="text" value = {city} onChange = { e => setCity(e.target.value)} />
+          <label>State</label>
+          <input type="text" value = {state} onChange = { e => setState(e.target.value)} />
+          <label>Zip</label>
+          <input type="text" value = {zip} onChange = { e => setZip(e.target.value)} />
+          <br></br>
+
           <input type="submit" />
         </form>
       {orders.map((order, idx) => {
         return(
           <div key={idx}>
-            {order.customer.firstname} {order.customer.lastname} - {order.product} - {order.price}
+            {order.customer.firstname} {order.customer.lastname} - {order.product} - {order.price} 
           </div>
         )
       })}
